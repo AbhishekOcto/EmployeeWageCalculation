d961a4ad750026f7613c5e90f0c209c93365fff7
Abhishek <you@example.com>
2022-07-29 19:53:56 +0530

    [Abhishek]: Added UC5 Wage for a month of 20 working days

--- src/com/bridgelabz/empWage/EmployeeWage.java
+++ src/com/bridgelabz/empWage/EmployeeWage.java
@@ -5,29 +5,31 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Calculation Program");
 		
-		//UC4
+		//added UC5
 		
 		int WagePerHour=20;
 		int FullDayHour=8;
 		int PartTimeHour=4;
+		int WorkingDays=20;
 		
+		for  (int day = 0; day <= WorkingDays; day++) {
 		int isPresent =(int) Math.floor(Math.random()*10) % 3;
-		
 		switch (isPresent) {
 		
 		case 0: 
 			System.out.println("Employee is Absent");
-			System.out.println("Employee Wage for a day is 0 ");
+			System.out.println("Employee Wage for a month is 0 ");
 			break;
 		case 1:
-			System.out.println("Employee is Present");
-			System.out.println("Employee Wage for a day is: " +WagePerHour*FullDayHour);
+			System.out.println("Employee is Present and Full time");
+			System.out.println("Employee's Wage for a month is " +WagePerHour*FullDayHour*WorkingDays);
 			break;
 		case 2:
-			System.out.println("Employee is doing part time");
-			System.out.println("Employee Wage for a day is: " +WagePerHour*PartTimeHour);
+			System.out.println("Employee is Present and doing part time");
+			System.out.println("Employee's part time Wage for a month is :" +WagePerHour*PartTimeHour*WorkingDays);
 			break;
-		}
+		    }
+     	 }
+  
 	}
-
 }
