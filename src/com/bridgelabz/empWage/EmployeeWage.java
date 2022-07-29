7bd73a8f2235c3247f4b81c0e90f07163e0fcf9b
Abhishek <you@example.com>
2022-07-29 19:03:04 +0530

    [Abhishek]:Added UC1 attendence check of employees

--- src/com/bridgelabz/empWage/EmployeeWage.java
+++ src/com/bridgelabz/empWage/EmployeeWage.java
@@ -4,6 +4,15 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Calculation Program");
+		
+		//added UC1
+		
+		int EmployeePresent=1;
+		int isPresent =(int) Math.floor(Math.random()*10) % 2;
+		if (isPresent == EmployeePresent)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
