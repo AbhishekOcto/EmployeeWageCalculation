da189e0eb3967dc845c3de6b7c5c4eb4587d201f
Abhishek <you@example.com>
2022-07-29 19:15:51 +0530

    [Abhishek]: Added UC3 Part time and Full time wages

--- src/com/bridgelabz/empWage/EmployeeWage.java
+++ src/com/bridgelabz/empWage/EmployeeWage.java
@@ -8,14 +8,21 @@ public class EmployeeWage {
 		//UC2
 		
 		int EmployeePresent=1;
+		int EmployeePartTime=2;
 		int WagePerHour=20;
 		int FullDayHour=8;
-		int isPresent =(int) Math.floor(Math.random()*10) % 2;
+		int PartTimeHour=4;
+		
+		int isPresent =(int) Math.floor(Math.random()*10) % 3;
 		if (isPresent == EmployeePresent) {
 			System.out.println("Employee is Present");
-			System.out.println("Employee wage for a day is " +WagePerHour*FullDayHour);
+			System.out.println("Employee wage for a day is : " +WagePerHour*FullDayHour);
 		}
-		else {
+		else if(isPresent == EmployeePartTime) {
+			System.out.println("Employee is doing part time");
+			System.out.println("Employee part time wage for a day is " +WagePerHour*PartTimeHour);
+		}
+		else  {
 			System.out.println("Employee is Absent");
 			System.out.println("Employee wage for a day is 0");
 		}
