0a819348382206794bf4b335f40f81ed2bf244dc
Abhishek <you@example.com>
2022-08-13 10:18:30 +0530

    Modified: [Abhishek] Added UC8 Compute Employee Wage

--- src/com/bridgelabz/empWage/EmployeeWage.java
+++ src/com/bridgelabz/empWage/EmployeeWage.java
@@ -1,61 +1,78 @@
 package com.bridgelabz.empWage;
 
 public class EmployeeWage {
-	public static final int EmployeePresent=1;
-	public static final int EmployeePartTime=2;
-	public static final int WagePerHour=20;
-	public static final int WorkingDays=20;
-	public static final int MonthMaxHours=100;
-
-	 public void wageCalculation() {
-		System.out.println("Welcome to Employee Wage Calculation Program");
-		int numberOfHours=0;
-		int nHours=0;
-		int totalWorkingDays=0, totalWorkingHours=0;
-		int totalEmployeeWage=0;
+	/* 
+	 * UC8
+	 * Compute Employee Wage for multiple companies
+	 * */
+	
+	public static final int EmployeePresent = 1;
+	public static final int EmployeePartTime = 2;
+	private final  String companyName;
+	private final  int WagePerHour;
+	private final  int WorkingDays;
+	private final  int MonthMaxHours;
+	
+		int numberOfHours = 0;
+		int nHours = 0;
+		int totalWorkingDays = 0; 
+		int totalWorkingHours = 0;
+		int totalEmployeeWage = 0;
 		
+		public EmployeeWage(String companyName, int WagePerHour, int WorkingDays, int MonthMaxHours) {
+			this.companyName = companyName;
+			this.WagePerHour = WagePerHour;
+			this.WorkingDays = WorkingDays;
+			this.MonthMaxHours = MonthMaxHours;
+				
+		}
 		
-		while(totalWorkingHours<MonthMaxHours && totalWorkingDays<WorkingDays) {
-		int isPresent =(int) Math.floor(Math.random()*10) % 3;
-		switch (isPresent) {
-		
-		case EmployeePresent: 
-			numberOfHours= 8;
-			totalWorkingHours= totalWorkingHours + numberOfHours;
-			System.out.println("Employee is Present");
-			System.out.println("Employee Wage for a day is "+numberOfHours*WagePerHour);
-			totalWorkingDays++;
-			nHours+=numberOfHours;
-			break;
+		public int wageCalculation() {	
 			
-		case EmployeePartTime:
-			numberOfHours= 4;
-			totalWorkingHours= totalWorkingHours + numberOfHours;
-			System.out.println("Employee is doing part time");
-			System.out.println("Employee part time wage for a day is "+numberOfHours*WagePerHour);
-			totalWorkingDays++;
-			nHours+=numberOfHours;
-			break;
-		
-		default :
-			numberOfHours= 0;
-			totalWorkingHours= totalWorkingHours + numberOfHours;
-			System.out.println("Employee is Absent");
-			System.out.println("Employee Wage for day is 0");
-			break;
+			while( totalWorkingHours <= MonthMaxHours && totalWorkingDays < WorkingDays ) {
+				
+				totalWorkingDays++;
+				
+				int isPresent = (int)Math.floor(Math.random()*10)%3;
+				
+				switch(isPresent) {
+				
+				case EmployeePartTime:
+					numberOfHours = 4;
+					break;
+				case EmployeePresent:
+					numberOfHours = 8;
+					break;
+				default:
+					numberOfHours = 0;
+					}
+				
+				totalWorkingHours += numberOfHours;
+				
+				System.out.println("Day : "+ totalWorkingDays + "; Employee hours for the day : " +numberOfHours);	
+				
 			}
-		System.out.println("\ndays  = " + totalWorkingDays + " No. of Hours = " +nHours);
-		System.out.println(	"\nEmployee Monthly Earn for Max"
-				+ " Days or Hours = " + ((numberOfHours) * WagePerHour) * WorkingDays);
+		   
+		return totalWorkingHours * WagePerHour;
 		
-		totalEmployeeWage= totalWorkingHours * WagePerHour;
-		System.out.println("\nTotal Employee Wage without Condition is "+ totalEmployeeWage);
 		}
-		}
+		
 	
      public static void main(String[] args) {
-	  EmployeeWage ew = new EmployeeWage(); 
-	  ew.wageCalculation();
-   
-	}
-}
+    	 System.out.println("Welcome to Employee Wage Calculation Program");
+	  
+    	 EmployeeWage dMart = new EmployeeWage("dMart", 20, 20, 150);
+    	 EmployeeWage Reliance = new EmployeeWage("Reliance", 25, 18, 150);
+    	 EmployeeWage Flipkart = new EmployeeWage("Flipkart", 28, 24, 172);
+    	 EmployeeWage Amazon = new EmployeeWage("Amazon ", 22, 22, 158);
+    	 EmployeeWage Google = new EmployeeWage("Google ", 30, 26, 160);
+    	 
+ 		System.out.println("Total EmpWage for Company " + dMart.companyName + " is : " +dMart.wageCalculation());
+ 		System.out.println("Total EmpWage for Company " + Reliance.companyName + " is : " + Reliance.wageCalculation());
+ 		System.out.println("Total EmpWage for Company " + Flipkart.companyName + " is : " + Flipkart.wageCalculation());
+ 		System.out.println("Total EmpWage for Company " + Amazon.companyName + " is : " + Amazon.wageCalculation());
+ 		System.out.println("Total EmpWage for Company " + Google.companyName + " is : " + Google.wageCalculation());
+ 		
+	    }
+     
+ }
